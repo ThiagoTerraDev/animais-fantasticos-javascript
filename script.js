5a5598ed6b425b636e2aa2de40457d6292e7c45a
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-04-02 22:45:00 -0300

    smooth scroll internal link

--- script.js
+++ script.js
@@ -39,3 +39,23 @@ function initAccordion() {
     }
 }
 initAccordion();
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+    
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+
+initScrollSuave();
