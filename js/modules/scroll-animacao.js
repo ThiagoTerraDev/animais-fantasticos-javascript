8f07977f46f4cde9b987a7faab91c926a388c327
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-04-26 10:30:45 -0300

    animating numbers

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -9,6 +9,8 @@ export default function initAnimacaoScroll() {
                 const isSectionVisible = (sectionTop - windowMetade) < 0;
                 if(isSectionVisible) {
                     section.classList.add('ativo');
+                } else if(section.classList.contains('ativo')) {
+                    section.classList.remove('ativo');
                 }
             });
         }
