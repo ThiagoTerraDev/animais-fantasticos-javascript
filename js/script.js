d85fc77ce9939998904230660851a4048bbce000
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-05-03 14:35:15 -0300

    animais fetch

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,5 +17,5 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
