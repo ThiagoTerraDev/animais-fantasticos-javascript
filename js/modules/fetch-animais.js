b21ce64c852ebcda49850d2dccf550986d4d07a5
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-05-03 17:08:35 -0300

    bitcoin fetch

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -2,14 +2,18 @@ import initAnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
     async function fetchAnimais(url) {
-        const animaisResponse = await fetch(url);
-        const animaisJSON = await animaisResponse.json();
-        const numerosGrid = document.querySelector('.numeros-grid');
-        animaisJSON.forEach(animal => {
-            const divAnimal = createAnimal(animal);
-            numerosGrid.appendChild(divAnimal);
-        })
-        initAnimaNumeros();
+        try {
+            const animaisResponse = await fetch(url);
+            const animaisJSON = await animaisResponse.json();
+            const numerosGrid = document.querySelector('.numeros-grid');
+            animaisJSON.forEach(animal => {
+                const divAnimal = createAnimal(animal);
+                numerosGrid.appendChild(divAnimal);
+            })
+            initAnimaNumeros();
+        } catch(erro) {
+            console.log(erro);
+        }
     }
     
     function createAnimal(animal) {    
