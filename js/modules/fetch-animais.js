0b542684c423fdaa1f0d6c1f8120372e1873feff
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-05-12 11:27:39 -0300

    transforming anima-numeros into a class

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
     async function fetchAnimais(url) {
@@ -9,8 +9,9 @@ export default function initFetchAnimais() {
             animaisJSON.forEach((animal) => {
                 const divAnimal = createAnimal(animal);
                 numerosGrid.appendChild(divAnimal);
-            })
-            initAnimaNumeros();
+            });
+            const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+            animaNumeros.init();
         } catch (erro) {
             console.log(erro);
         }
