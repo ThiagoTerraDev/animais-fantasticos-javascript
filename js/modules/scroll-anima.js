b5a539598c5a1757a6e3c7e16f77bc6ac974bb70
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-05-13 16:52:04 -0300

    adding debounce on scroll-anima

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,9 +1,11 @@
+import debounce from "./debounce.js";
+
 export default class ScrollAnima {
     constructor(sections) {
         this.sections = document.querySelectorAll(sections);
         this.windowMetade = window.innerHeight * 0.6;
         
-        this.checkDistance = this.checkDistance.bind(this);
+        this.checkDistance = debounce(this.checkDistance.bind(this), 50);
     }
 
     // Pega a distância de cada item em relação ao topo do site.
@@ -18,7 +20,7 @@ export default class ScrollAnima {
     }
 
     // Verifica a distância em cada objeto em relação ao scroll do site.
-    checkDistance() {
+    checkDistance() {        
         this.distance.forEach((item) => {
             if (window.pageYOffset > item.offset) {
                 item.element.classList.add('ativo');
