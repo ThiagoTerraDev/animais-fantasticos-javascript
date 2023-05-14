a1d1bd3def046dd722a9a72ecff0bd4fac2f8f99
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-05-13 22:38:59 -0300

    animaisapi fix5

--- js/script.js
+++ js/script.js
@@ -36,6 +36,6 @@ menuMobile.init();
 
 initFuncionamento();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('../../animaisapi2.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
