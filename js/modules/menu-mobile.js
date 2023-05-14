c1c8f5f82f82a77cc16a455bbe73db8fa9dbe7ef
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-05-13 21:09:40 -0300

    menu mobile refactored

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,20 +1,35 @@
 import outsideClick from './outside-click.js';
 
-export default function initMenuMobile() {
-    const menuButton = document.querySelector('[data-menu="button"]');
-    const menuList = document.querySelector('[data-menu="list"]');
-    const eventos = ['click', 'touchstart'];
+export default class MenuMobile {
+    constructor(menuButton, menuList, events) {
+        this.menuButton = document.querySelector(menuButton);
+        this.menuList = document.querySelector(menuList);
+        this.activeClass = 'active';
 
-    function openMenu() {
-        menuList.classList.add('active');
-        menuButton.classList.add('active');
-        outsideClick(menuList, eventos, () => {
-            menuList.classList.remove('active');
-            menuButton.classList.remove('active');
+         // Define touchstart e click como argumento padrão de events caso o usuário (da função) não defina.
+         if (events === undefined) this.events = ['touchstart', 'click'];
+         else this.events = events;     
+         
+         this.openMenu = this.openMenu.bind(this);
+    }
+
+    openMenu() {
+        this.menuList.classList.add(this.activeClass);
+        this.menuButton.classList.add(this.activeClass);
+        outsideClick(this.menuList, this.events, () => {
+            this.menuList.classList.remove(this.activeClass);
+            this.menuButton.classList.remove(this.activeClass);
         });
     }
+
+    addMenuMobileEvents() {
+        this.events.forEach(evento => this.menuButton.addEventListener(evento, this.openMenu));
+    }
     
-    if (menuButton) {            
-        eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+    init() {
+        if (this.menuButton && this.menuList) {
+            this.addMenuMobileEvents();
+        }
+        return this;
     }
 }
