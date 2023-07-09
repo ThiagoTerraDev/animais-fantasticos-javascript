cd62c66cebb3ec3cc5c9ee5d3500f697ca300508
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-07-09 16:19:02 -0300

    API path fixed

--- js/script.js
+++ js/script.js
@@ -37,6 +37,6 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../animaisapi2.json', '.numeros-grid');
+fetchAnimais('./animaisapi2.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
