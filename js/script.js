83848533e533f91e7c6e160929fd046924fc84b9
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-07-11 10:34:00 -0300

    correcoes gerais de bugs

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../animaisapi2.json', '.numeros-grid');
+fetchAnimais('./animaisapi2.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
